7071839dcfb2fa6315d35a32faf1bc4ea0ac0fe2
Mariusz Nowak <medyk@medikoo.com>
2015-11-12 17:31:30 +0100

    Fix base server model

--- server/model.js
+++ server/model.js
@@ -9,6 +9,7 @@ require('dbjs-ext/string/string-line/password')(db);
 require('../model/user/roles');
 require('../model/institutions');
 
-require('../model/business-process');
+// TODO: Remove below line as soon as first business process is configured
+require('../model/business-process/base');
 
 require('../model/views');
