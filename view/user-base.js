e9a0d6ad4ec5097e7069b795f6fab2fe82a6cc71
Mariusz Nowak <medyk@medikoo.com>
2015-11-12 13:00:02 +0100

    Expose user button in user-base.js

--- view/user-base.js
+++ view/user-base.js
@@ -2,7 +2,8 @@
 
 'use strict';
 
-var db = require('mano').db;
+var startsWith = require('es5-ext/string/#/starts-with')
+  , db         = require('mano').db;
 
 // Assure base customisations are loaded
 require('./base');
@@ -14,6 +15,13 @@ exports._submittedMenu = function (context) {
 	var user = context.user;
 	return list(user.roles, function (role) {
 		var roleTitle = db.Role.meta[role].label;
+
+		if (role === 'user' && startsWith.call(context.appName, 'business-process-')) {
+			return li(form({ method: 'post', action: '/change-business-process/' },
+				input({ type: 'hidden',
+					name: user.__id__ + '/currentBusinessProcess', value: null }),
+				button({ type: 'submit' }, roleTitle)));
+		}
 
 		if (user.currentRoleResolved === role) {
 			return li({ class: 'submitted-menu-item-active' }, a({ href: '/' }, roleTitle));
