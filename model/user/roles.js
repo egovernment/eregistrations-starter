914c4e59059dac8111f1eddc53f8e2bfe5d15632
Mariusz Nowak <medyk@medikoo.com>
2015-11-13 10:27:13 +0100

    Remove statistics from model

--- model/user/roles.js
+++ model/user/roles.js
@@ -8,11 +8,9 @@ var _  = require('../../i18n')
 module.exports = require('eregistrations/model/user')(db);
 
 db.Role.members.add('metaAdmin');
-db.Role.members.add('statistics');
 db.Role.members.add('user');
 db.Role.members.add('usersAdmin');
 
 db.Role.meta.get('metaAdmin').label = _("Meta Admin");
-db.Role.meta.get('statistics').label = _("Statistics");
 db.Role.meta.get('user').label = _("User");
 db.Role.meta.get('usersAdmin').label = _("Users Admin");
