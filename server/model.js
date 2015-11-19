6e4a5e1f6760ed0d7b974e980df9b0b8b1434f95
Mariusz Nowak <medyk@medikoo.com>
2015-11-19 14:42:02 +0100

    Ensure domId property on sections

--- server/model.js
+++ server/model.js
@@ -12,4 +12,7 @@ require('../model/institutions');
 // TODO: Remove below line as soon as first business process is configured
 require('../model/business-process/base');
 
+// Ensure domId property on sections
+require('eregistrations/view/dbjs/form-section-base-dom-id');
+
 require('../model/views');
