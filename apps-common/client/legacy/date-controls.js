309e35beee84059c52a3b9c82cb0dea7355ed563
Marek Tuchowski <marek@tuchowski.com.pl>
2015-11-20 13:07:26 +0100

    Issue #3. Removed apps-common/client/legacy/date-controls.js.

--- apps-common/client/legacy/date-controls.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var dateControls = require('mano-legacy/live/date-controls');
-
-dateControls.order = ['d', 'm', 'y'];
-dateControls.labels = { year: 'AAAA', month: 'MM', day: 'DD' };
