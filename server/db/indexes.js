1d504301a7ddc0e9c67a06434b656539e20a9412
Mariusz Nowak <medyk@medikoo.com>
2015-12-04 15:40:32 +0100

    Update to use new app resolver

--- server/db/indexes.js
+++ server/db/indexes.js
@@ -2,4 +2,19 @@
 
 'use strict';
 
-// TODO: to be configured on the go
+var isFalsy           = require('eregistrations/utils/is-falsy')
+  , businessProcesses = require('../../apps-common/business-processes-submitted')
+  , driver            = require('mano').dbDriver
+
+  , db = driver.db;
+
+// App resolution
+driver.indexKeyPath('appAccessId', db.User.filterByKey('password')).done();
+driver.indexKeyPath('appName', db.User.filterByKey('password')).done();
+
+// Users table
+driver.indexCollection('isActiveAccount',
+	db.User.filterByKey('password').filterByKey('isDemo', isFalsy)).done();
+
+// Business processes table
+driver.indexKeyPath('searchString', businessProcesses).done();
