122a1f86323897f177b86a770cfa7dec1c24a97f
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 16:21:27 +0200

    Add recentlyVisited configuration

---
+++ model/user/recently-visited/business-processes.js
@@ -0,0 +1,22 @@
+// Predefined groups of recentlyVisited.businessProcesses collections
+// Those collections will be LRU handled to provide full data for
+// recently visited business processes
+
+'use strict';
+
+var forEach             = require('es5-ext/object/for-each')
+  , db                  = require('../../../db')
+  , processingStepsMeta = require('../../../apps-common/processing-steps/meta');
+
+var recentlyVisited = module.exports =
+	require('eregistrations/model/user/recently-visited/business-processes')(db);
+
+forEach(processingStepsMeta, function (meta, stepShortPath) {
+	var pathTokens = stepShortPath.split('/'), target = recentlyVisited, token;
+	while (pathTokens.length > 1) {
+		token = pathTokens.shift();
+		if (!target[token]) target.define(token, { type: db.Object, nested: true });
+		target = target[token];
+	}
+	target.define(pathTokens[0], { multiple: true });
+});
