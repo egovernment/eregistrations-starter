9aadcf34e5e2dc6f4513d4556f57d017bda50207
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 15:46:48 +0200

    Update routers configuration

--- server/apps/routers.js
+++ server/apps/routers.js
@@ -5,5 +5,9 @@
 var ControllerRouter = require('controller-router');
 
 module.exports = {
-	'meta-admin': new ControllerRouter(require('../../apps/meta-admin/server/routes'))
+	'business-process-submitted':
+		new ControllerRouter(require('../../apps/business-process-submitted/server/routes')),
+	'meta-admin': new ControllerRouter(require('../../apps/meta-admin/server/routes')),
+	user: new ControllerRouter(require('../../apps/user/server/routes')),
+	'users-admin': new ControllerRouter(require('../../apps/users-admin/server/routes'))
 };
