238472c030a4247e13fa67a96df752a38a4f30c1
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 16:22:15 +0200

    Update server model configuration

--- server/model.js
+++ server/model.js
@@ -7,6 +7,7 @@ var db = module.exports = require('../db');
 require('dbjs-ext/string/string-line/password')(db);
 
 require('../model/user/roles');
+require('../model/user/recently-visited/business-processes');
 require('../model/institutions');
 
 // TODO: Remove below line as soon as first business process is configured
