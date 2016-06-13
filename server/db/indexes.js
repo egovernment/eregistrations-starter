2259acf8ace17df644817f03723cfbb94e10a1d8
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 15:55:11 +0200

    Update indexes

--- server/db/indexes.js
+++ server/db/indexes.js
@@ -2,19 +2,78 @@
 
 'use strict';
 
-var isFalsy           = require('eregistrations/utils/is-falsy')
-  , businessProcesses = require('../../apps-common/business-processes-submitted')
-  , driver            = require('mano').dbDriver
+var forEach                    = require('es5-ext/object/for-each')
+  , capitalize                 = require('es5-ext/string/#/capitalize')
+  , Set                        = require('es6-set')
+  , mano                       = require('mano')
+  , db                         = require('../../db')
+  , businessProcesses          = require('../../apps-common/business-processes')
+  , businessProcessesSubmitted = require('../../apps-common/business-processes/submitted')
+  , processingSteps            = require('../../apps-common/processing-steps')
+  , businessProcessStorages    = require('../business-process-storages')
 
-  , db = driver.db;
+  , driver = mano.dbDriver
+
+  , userAccounts = db.User.filterByKey('email').filterByKey('password')
+  , visitableUsers = userAccounts.or(db.User.filterByKey('isDemo', true))
+  , managerClients = db.User.filterByKey('manager')
+  , managers = userAccounts.filterByKey('roles', function (roles) { return roles.has('manager'); })
+  , userStorage = driver.getStorage('user');
 
 // App resolution
-driver.indexKeyPath('appAccessId', db.User.filterByKey('password')).done();
-driver.indexKeyPath('appName', db.User.filterByKey('password')).done();
+userStorage.indexKeyPath('appAccessId', visitableUsers).done();
+userStorage.indexKeyPath('appName', visitableUsers).done();
+// Resolved roles is used in server GET routes conf map resolver
+userStorage.indexKeyPath('currentRoleResolved', userAccounts.filterByKey('currentRoleResolved'))
+	.done();
 
 // Users table
-driver.indexCollection('isActiveAccount',
-	db.User.filterByKey('password').filterByKey('isDemo', isFalsy)).done();
+userStorage.indexCollection('isActiveAccount', userAccounts).done();
+userStorage.indexKeyPath('fullName', userAccounts).done();
 
-// Business processes table
-driver.indexKeyPath('searchString', businessProcesses).done();
+userStorage.indexKeyPath('managerDataForms/progress',
+	managers.filterByKeyPath('managerDataForms/progress', 1)).done();
+userStorage.indexKeyPath('isManagerActive',
+	managers.filterByKey('isManagerActive', true)).done();
+userStorage.indexKeyPath('isManagerDesctructionBlocker', managerClients).done();
+userStorage.indexKeyPath('canManagedUserBeDestroyed', managerClients).done();
+userStorage.indexKeyPath('canManagerBeDestroyed', managers).done();
+
+userStorage.indexKeyPath('searchString', userAccounts).done();
+
+forEach(businessProcessStorages, function (storage, name) {
+	// Needed for My Account
+	storage.indexKeyPath('status', businessProcesses[name]).done();
+	storage.indexKeyPath('certificates/applicable', businessProcesses[name]).done();
+	// Needed for Part B search
+	storage.indexKeyPath('searchString', businessProcessesSubmitted[name]).done();
+	// Needed for print forms generator
+	storage.indexKeyPath('dataForms/lastEditStamp', businessProcesses[name]).done();
+});
+
+// Statuses and sort indexes
+forEach(processingSteps, function (meta, stepShortPath) {
+	var keyPaths = new Set()
+	  , storages = {};
+
+	meta._services.forEach(function (name) { storages[name] = businessProcessStorages[name]; });
+
+	forEach(meta, function (conf) { keyPaths.add(conf.indexName); });
+
+	keyPaths.forEach(function (keyPath) {
+		forEach(storages, function (storage, name) {
+			storage.indexKeyPath(keyPath, businessProcessesSubmitted[name]
+				.filterByKeyPath(meta.all.indexName, meta.all.indexValue)).done();
+		});
+	});
+});
+
+// Computed properties
+require('../../apps-common/business-process-list-computed-properties').forEach(function (keyPath) {
+	forEach(businessProcessStorages, function (storage, name) {
+		if (storage._indexes[keyPath]) return; // Already configured
+		// Filter not applicable to given service
+		if (!db['BusinessProcess' + capitalize.call(name)].prototype.resolveSKeyPath(keyPath)) return;
+		storage.indexKeyPath(keyPath, businessProcessesSubmitted[name]).done();
+	});
+});
