f9617fd97b73bb75129e713fc6ecf06a63fa6523
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 15:58:59 +0200

    Update server views handling

--- server/db/views.js
+++ server/db/views.js
@@ -1,29 +1,63 @@
 // Populate predefined view objects with data
 // (it's about providing (reactive way) state of first pages on business processes lists)
 
-// See example configurations at:
-// https://github.com/egovernment/eregistrations-lomas/blob/master/server/db/views.js
-
 'use strict';
 
-var toNaturalNumber   = require('es5-ext/number/to-pos-integer')
-  , getCompare        = require('eregistrations/utils/get-compare')
-  , serialize         = require('eregistrations/utils/db-view/serialize')
-  , db                = require('../../db')
-  , users             = require('eregistrations/users/index.js')
-  , getUserOrderIndex = require('eregistrations/users/get-default-order-index')
-  , envItemsPerPage   = toNaturalNumber(require('../../env').objectsListItemsPerPage)
+var assign                  = require('es5-ext/object/assign')
+  , deferred                = require('deferred')
+  , memoize                 = require('memoizee')
+  , debug                   = require('debug-ext')('view-snapshot')
+  , getDbSet                = require('eregistrations/server/utils/get-db-set')
+  , getDbArray              = require('eregistrations/server/utils/get-db-array')
+  , trackFirstPage          = require('eregistrations/server/view-tracker/first-page')
+  , trackStep               = require('eregistrations/server/view-tracker/processing-step')
+  , driver                  = require('mano').dbDriver
+  , processingStepsMeta     = require('../../apps-common/processing-steps/meta')
+  , businessProcessStorages = require('../business-process-storages')
 
-  , pageLimit = envItemsPerPage || require('eregistrations/conf/objects-list-items-per-page');
+  , stringify = JSON.stringify
+  , resolved = deferred(null)
+  , userStorage = driver.getStorage('user')
+  , reducedStorage = driver.getReducedStorage();
 
-var bindCollection = function (collection, snapshot, getIndex) {
-	var slice = collection.toArray(getCompare(getIndex)).slice(0, pageLimit)
-	  , view = serialize(slice, getIndex);
-	if (snapshot.totalSize !== collection.size) snapshot.totalSize = collection.size;
-	collection._size.on('change', function (event) { snapshot.totalSize = collection.size; });
-	if (view !== snapshot.get(1)) snapshot.set(1, view);
-	slice.on('change', function () { snapshot.set(1, serialize(slice, getIndex)); });
+var trackerOptions = {
+	userStorage: userStorage,
+	reducedStorage: reducedStorage,
+	itemsPerPage: require('../../env').objectsListItemsPerPage
 };
 
-// Users Admin
-bindCollection(users, db.views.usersAdmin, getUserOrderIndex);
+module.exports = memoize(function (viewPath) {
+	var tokens = viewPath.split('/'), stepShortPath, meta, storages;
+
+	debug('intialize %s', viewPath);
+
+	if (viewPath === 'usersAdmin') {
+		// Users Admin
+		return getDbSet(userStorage, 'computed', 'isActiveAccount', '11')(function (set) {
+			return getDbArray(set, userStorage, 'direct', null)(function (array) {
+				return trackFirstPage(userStorage, 'usersAdmin', set, array, trackerOptions);
+			});
+		});
+	}
+
+	if (tokens[0] === 'businessProcesses') {
+		tokens.shift();
+		if (tokens.length !== 1) {
+			console.error("\n\nUnrecognized view path " + stringify(viewPath) + "\n\n");
+			return resolved;
+		}
+		stepShortPath = tokens.shift();
+		meta = processingStepsMeta[stepShortPath];
+		if (meta) {
+			storages = meta._services.map(function (name) {
+				return businessProcessStorages[name];
+			});
+			return trackStep(stepShortPath, meta, assign({
+				businessProcessStorage: storages
+			}, trackerOptions));
+		}
+		console.error("\n\nUnrecognized view path " + stringify(viewPath) + "\n\n");
+		return resolved;
+	}
+	console.error("\n\nUnrecognized view path " + stringify(viewPath) + "\n\n");
+}, { primitive: true });
