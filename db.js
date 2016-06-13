582ee309b917447a04c2a22a57d2663dd0a7dedd
Mariusz Nowak <medyk@medikoo.com>
2016-06-13 15:04:36 +0200

    Update db initialization

--- db.js
+++ db.js
@@ -2,16 +2,6 @@
 
 'use strict';
 
-var Database = require('dbjs')
-  , _        = require('./i18n')
-  , d        = require('d');
+var db = module.exports = require('eregistrations/db');
 
-var db = module.exports = require('mano').db = new Database();
-
-db.locale = 'en';
-
-Object.defineProperty(db.Base, 'chooseLabel', d(_("Choose:")));
-Object.defineProperties(db.Boolean, {
-	trueLabel: d(_("Yes")),
-	falseLabel: d(_("No"))
-});
+db.locale = 'en-GB';
